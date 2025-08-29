7f90560a46a196b6adeee3f9921fe3f58ebf28cf
yamada2450 <ttc2450sd0014@edu.tech.ac.jp>
2025-08-29 18:33:31 +0900

    Add files via upload

---
+++ table/script.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    $("td").hover(function() {
+
+        $(this).parent().addClass("target");
+
+
+        let myIndex = $(this).index();
+
+
+        myIndex++;
+
+
+        $("td:nth-child(" + myIndex + ")").addClass("target");
+    }, function() {
+
+        $(".target").removeClass("target");
+    });
+});
